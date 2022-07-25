65cac6468961ea75558ec7281ba2a7ea15d685b7
Ravi_O <ravimurugan051@gmail.com>
2022-07-25 19:04:21 +0530

    db hitted

--- Hibernate-Demo-1/src/main/java/Starter/Application_Starter.java
+++ Hibernate-Demo-1/src/main/java/Starter/Application_Starter.java
@@ -2,7 +2,10 @@ package Starter;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import tut.hibernate.modal.Person;
 
 public class Application_Starter {
 
@@ -10,6 +13,12 @@ public class Application_Starter {
 
 		EntityManagerFactory entityMangerFactory = Persistence.createEntityManagerFactory("tut.hibernate.modal");
 		EntityManager entityManager = entityMangerFactory.createEntityManager();
+		EntityTransaction entityTranscation = entityManager.getTransaction();
+		entityTranscation.begin();
+		Person p = new Person((long) 1,"ravi","chandran");
+		entityManager.persist(p);
+		entityTranscation.commit();
+		entityManager.close();
 		
 	}
 
--- Hibernate-Demo-1/src/main/java/tut/hibernate/modal/Person.java
+++ Hibernate-Demo-1/src/main/java/tut/hibernate/modal/Person.java
@@ -3,10 +3,12 @@ package tut.hibernate.modal;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class Person {
 	
 	@Id
