8e6962dc34863c22840220fea3d5b06043fad619
Ravi <ravimurugan051@gmail.com>
2022-08-04 20:16:40 +0530

    add h

--- Hibernate-Demo-1/src/main/java/Starter/Application_Starter.java
+++ Hibernate-Demo-1/src/main/java/Starter/Application_Starter.java
@@ -1,25 +1,39 @@
 package Starter;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import tut.hibernate.modal.Person;
 
 public class Application_Starter {
 
+	@SuppressWarnings({ "unchecked", "unused" })
 	public static void main(String[] args) {
 
 		EntityManagerFactory entityMangerFactory = Persistence.createEntityManagerFactory("tut.hibernate.modal");
 		EntityManager entityManager = entityMangerFactory.createEntityManager();
 		EntityTransaction entityTranscation = entityManager.getTransaction();
 		entityTranscation.begin();
-		Person p = new Person((long) 1,"ravi","chandran");
+		Person p = new Person();
+		p.setFirstName("Ravi");
+		p.setLastName("Chandran");
 		entityManager.persist(p);
 		entityTranscation.commit();
+		Query query =entityManager.createQuery("from Person");
+		List<Person> list = query.getResultList();
+		Iterator<Person> ir = list.iterator();
+		for (Person person : list) {
+			System.out.println(person.getFirstName());
+		}
+		System.out.println();
 		entityManager.close();
-		
+
 	}
 
 }
--- Hibernate-Demo-1/src/main/java/tut/hibernate/modal/Person.java
+++ Hibernate-Demo-1/src/main/java/tut/hibernate/modal/Person.java
@@ -1,17 +1,21 @@
 package tut.hibernate.modal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
+
 public class Person {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String firstName;
 	private String lastName;
